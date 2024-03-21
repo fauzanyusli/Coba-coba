e2e8c2a2c18b5d029ef883387da762ce89baab88
WP2_FauzanYusliHamid <fauzanyuslihamid@gmail.com>
2024-03-21 21:24:57 +0700

    COBA COBA AJA BRAY

---
+++ contoh05.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Contoh Script PHP</title>
+</head>
+<body>
+<?php
+
+$nim="12170829";
+$nama="Bima Bintang Galaxy";
+$kelas="12.1A.01";
+    
+echo "Nim Saya=$nim<br>";
+echo "Nama Saya=$nama<br>";
+echo "Kelas Saya=$kelas<br>";
+?>
+</body>
+</html>
---
+++ contoh051.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Contoh Sqript PHP</title>
+</head>
+<?php
+
+$nim="12345678";
+$nama="Bima";
+$kelas="121a";
+
+echo "Nim Saya=$nim<br>";
+echo "Nama Saya=$nama<br>";
+echo "Kelas Saya=$kelas<br>";
+
+?>
+</body>
+</html>
